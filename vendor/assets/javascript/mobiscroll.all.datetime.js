620323054e69c911b86216696adb2f781272ea04
Mike Chen <chagel@gmail.com>
2015-12-15 18:08:30 +0800

    add js/css entry files

---
+++ vendor/assets/javascript/mobiscroll.all.datetime.js
@@ -0,0 +1,6 @@
+//= require mobiscroll.core.js
+//= require mobiscroll.frame.js
+//= require mobiscroll.scroller.js
+//= require mobiscroll.util.datetime.js
+//= require mobiscroll.datetimebase.js
+//= require mobiscroll.datetime.js
